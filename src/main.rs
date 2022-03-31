7d6422b5aeb3bd0d74dcfd222aa556bd17efe697
Bruno Oliveira <brunosoe@motorola.com>
2022-03-31 11:18:51 -0300

    adding int to float

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 //print hello world in rust
 fn main() {
     // start a int with value 10
-    let x = 10;
+    let mut x = 10;
     println!("x = {}", x);
 
     x = 20;
     println!("x = {}", x);
+    // end of immutable variable commit
+
+    // start a float with value 10.5
+
+    let y: f64 = 10.5;
+    let sum = x + y;
+    println!("sum = {}", sum);
 }
